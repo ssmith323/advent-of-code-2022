2b58e884e6a83941e8cc277bcc050ad120b8074f
Stephen J Smith <stephen.smith@jasper.ai>
2022-12-03 08:36:56 -0500

    updating day 3

--- workspaces/day03/src/part2.js
+++ workspaces/day03/src/part2.js
@@ -3,12 +3,11 @@ const letters = require('./letters.js');
 const { processGroupRucksack } = require('./util.js');
 require('@util/array');
 
-const part2 = (filename) => {
-  return getContents(filename)
+const part2 = (filename) =>
+  getContents(filename)
     .match(/(?:^.*$\n?){1,3}/gm)
     .map(processGroupRucksack)
     .map((letter) => letters[letter])
     .sum();
-};
 
 module.exports = { part2 };
