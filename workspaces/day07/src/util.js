e195c55ffbf0ddf65ce2e2175780300c59d7239d
Stephen J Smith <stephen.smith@jasper.ai>
2022-12-07 09:48:46 -0500

    clean up day 7

--- workspaces/day07/src/util.js
+++ workspaces/day07/src/util.js
@@ -1,25 +1,36 @@
+const handleCd = (pwd, dir) => {
+  if (dir === '..') {
+    pwd.pop();
+  } else {
+    pwd.push(dir);
+  }
+};
+
+const handleFile = ([pwd, sizes], size) => {
+  for (let i = 1; i <= pwd.length; i++) {
+    sizes[pwd.slice(0, i).join('/')] += parseInt(size, 10);
+  }
+};
+
+const handleDir = ([pwd, sizes], dirName) => {
+  const path = [...pwd, dirName].join('/');
+  return { ...sizes, [path]: 0 };
+};
+
+// eslint-disable-next-line max-lines-per-function
 const parseInput = (arr) => {
   const pwd = [];
   let sizes = { '/': 0 };
   arr.forEach((a) => {
     const line = a.split(' ');
     if (line[0] === '$') {
-      switch (line[1]) {
-        case 'cd':
-          if (line[2] === '..') {
-            pwd.pop();
-          } else {
-            pwd.push(line[2]);
-          }
+      if (line[1] === 'cd') {
+        handleCd(pwd, line[2]);
       }
     } else if (line[0] === 'dir') {
-      const path = [...pwd, line[1]].join('/');
-      sizes = { ...sizes, [path]: 0 };
+      sizes = handleDir([pwd, sizes], line[1]);
     } else {
-      for (let i = 1; i < pwd.length; i++) {
-        sizes[pwd.slice(0, i).join('/')] += parseInt(line[0], 10);
-      }
-      sizes[pwd.join('/')] += parseInt(line[0], 10);
+      handleFile([pwd, sizes], line[0]);
     }
   });
   return sizes;
